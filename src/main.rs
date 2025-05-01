b5f6ea109aaf8f152ef0c09569781c863b9d6e57
Victor Augusto Guimarães <v.augusto@estudantes.ifg.edu.br>
2025-05-01 10:34:45 -0300

    feat: Configuração inicial do SDL com Rust (#6)
    * feat: init setup
    
    * Update Cargo.toml
    
    ---------
    
    Co-authored-by: Wesley Oliveira <wesleyfetish@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,62 @@
+
 mod memory;
 
 use memory::bus::Bus;
 
+use std::ops::Not;
+
+use std::{
+	ffi::{CStr, CString},
+	mem::zeroed,
+};
+
+use sdl3_sys::{
+    error::SDL_GetError,
+    init::{SDL_INIT_VIDEO, SDL_Init, SDL_Quit},
+    video::{SDL_CreateWindow, SDL_DestroyWindow, SDL_WINDOW_OPENGL},
+    events::{
+	SDL_PollEvent,
+	SDL_EVENT_QUIT,
+    }
+};
+
 fn main() {
-    let mut bus = Bus::new();
+    unsafe {
+        std::env::set_var("SDL_VIDEODRIVER", "windows");
+    }
 
-    // Simular a escrita do opcode `LDA #0x42` (0xA9) em 0x0000 e o operando 0x42 em 0x0001
-    let addr = 0x0000;
+    if unsafe { SDL_Init(SDL_INIT_VIDEO).not() } {
+        panic!("SDL_Init failed: {:?}", unsafe {
+            CStr::from_ptr(SDL_GetError())
+        });
+    }
 
-    bus.write(addr, 0xA9);
-    bus.write(addr + 1, 1);
+    // Create window
+    let title = CString::new("SDL3 Bindgen Example").unwrap();
+    let window = unsafe { SDL_CreateWindow(title.as_ptr(), 800, 600, SDL_WINDOW_OPENGL) };
 
-    // Verificar se foi escrito corretamente
-    let op = bus.read(addr);
-    let val = bus.read(addr + 1);
+    if window.is_null() {
+        panic!(
+            "SDL_CreateWindow failed: {:?}",
+            unsafe {CStr::from_ptr(SDL_GetError()) }
+        );
+    }
 
-    println!("Opcode na RAM: {:#X}", op);
-    println!("Operando na RAM: {:#X}", val);
+    // Main loop
+    let mut event = unsafe { zeroed() };
+    let mut running = true;
+    while running {
+        while unsafe {SDL_PollEvent(&mut event).not()} {
+            if unsafe {event.r#type == SDL_EVENT_QUIT.into()} {
+                running = false;
+            }
+        }
+
+        // Add rendering here
+    }
+
+    // Cleanup
+    unsafe {SDL_DestroyWindow(window)};
+    unsafe {SDL_Quit()};
+
 }
