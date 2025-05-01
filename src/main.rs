8a5b8918156fd4a89c19ae2d20d3a6247593457a
Wesley Oliveira <wesleyfetish@gmail.com>
2025-05-01 10:27:16 -0300

    Feature/simulacao nes memoria (#5)
    * feat: implementado mapeamento de memoria basico
    
    * fix: corrigido nome do artefato
    
    * fix: adicionado verificação para disparar a release apenas quando for a main
    
    * fix: gerar artefato apenas quando for para a main
    
    * chore: atualizado versão do projeto
    
    * feat(memory): criado modulo memory/bus.rs
    
    * feat: adicionado regiões de memoria nova e generalizado o método de mirroring
    
    * fix: corrigido typo
    
    * fix: fazoL
    
    * fix: corrigido main com write

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,15 @@
-type NesBus = Bus<2048, 0x8000>;
+mod memory;
 
-const fn mirror_ram(addr: u16) -> usize {
-    (addr & 0x07FF) as usize
-}
-
-struct Bus<const N: usize, const M: usize> {
-    ram: [u8; N],
-    pgrom: [u8; M], // 32 KiB = 0xFFFF - 0x8000 + 1
-}
-
-impl<const N: usize, const M: usize> Bus<N, M> {
-    fn new() -> Self {
-        Bus {
-            ram: [0; N],
-            pgrom: [0; M],
-        }
-    }
-
-    #[inline(always)]
-    fn read(&self, addr: u16) -> u8 {
-        match addr {
-            0x0000..=0x1FFF => self.ram[mirror_ram(addr)], // RAM + mirrors
-            0x8000..=0xFFFF => self.pgrom[(addr - 0x8000) as usize], // PRG-ROM
-            _ => {
-                eprintln!("Tentativa de acesso inválido: {:#X}", addr);
-                0xFF
-            }
-        }
-    }
-
-    #[inline(always)]
-    fn write(&mut self, addr: u16, value: u8) {
-        match addr {
-            0x0000..=0x1FFF => self.ram[mirror_ram(addr)] = value,
-            _ => {
-                eprintln!("Tentativa de acesso inválido: {:#X}", addr);
-            }
-        }
-    }
-}
+use memory::bus::Bus;
 
 fn main() {
-    let mut bus = NesBus::new();
+    let mut bus = Bus::new();
 
     // Simular a escrita do opcode `LDA #0x42` (0xA9) em 0x0000 e o operando 0x42 em 0x0001
     let addr = 0x0000;
 
-    bus.write(addr, 0xA9); // opcode LDA #imediato
-    bus.write(addr + 1, 0x42); // valor imediato (0x42)
+    bus.write(addr, 0xA9);
+    bus.write(addr + 1, 1);
 
     // Verificar se foi escrito corretamente
     let op = bus.read(addr);
