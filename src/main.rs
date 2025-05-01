d7bd6789ff86c1229a84e765582b812da42e11c8
Wesley Oliveira <wesleyfetish@gmail.com>
2025-05-01 10:26:18 -0300

    fix: corrigido main com write

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,17 @@ mod memory;
 use memory::bus::Bus;
 
 fn main() {
-    let bus = Bus::new();
+    let mut bus = Bus::new();
 
     // Simular a escrita do opcode `LDA #0x42` (0xA9) em 0x0000 e o operando 0x42 em 0x0001
     let addr = 0x0000;
 
+    bus.write(addr, 0xA9);
+    bus.write(addr + 1, 1);
+
     // Verificar se foi escrito corretamente
     let op = bus.read(addr);
     let val = bus.read(addr + 1);
-
-    println!("Opcode na RAM: {:#X}", op);
-    println!("Operando na RAM: {:#X}", val);
-
-    let op = bus.read(0x8000);
-    let val = bus.read(0x8000 + 1);
 
     println!("Opcode na RAM: {:#X}", op);
     println!("Operando na RAM: {:#X}", val);
