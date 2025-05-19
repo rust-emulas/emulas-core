715773fe4e41ccf0c7c5cc3408563fa8d5c78fa2
cangerana <cangerana@mrjoaovictorc@gmail.com>
2025-05-19 19:45:18 -0300

    readability improvements

--- src/sys/errors.rs
+++ src/sys/errors.rs
@@ -19,56 +19,38 @@ impl PartialEq for Extension {
    fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (Extension::NES, Extension::NES) => true,
+            (Extension::InvalidExtension, Extension::InvalidExtension) => true,
             _ => false,
         }
     }
 }
 
 pub struct InvalidROMFile;
-pub struct ErrorOnOpen;
-pub struct ErrorOnRead;
+pub struct ErrorOpeningROMFile;
+pub struct ErrorReadingROMFile;
 
 // TODO improve error names
 #[derive(Debug)]
 pub enum FileErrors {
     InvalidROMFile,
-    ErrorOnOpen,
-    ErrorOnRead,
+    ErrorOpeningROMFile,
+    ErrorReadingROMFile,
 }
 
-// TODO transforme this in generic
-impl fmt::Display for InvalidROMFile {
+trait FileErrorMessage {}
+
+impl fmt::Display for dyn FileErrorMessage {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "File invalid as NES ROM!")
     }
 }
 
-impl fmt::Debug for InvalidROMFile {
+impl fmt::Debug for dyn FileErrorMessage {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{{ File: {} at line {} }}", file!(), line!())
     }
 }
 
-impl fmt::Display for ErrorOnOpen {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Error when tried to open ROM file")
-    }
-}
-
-impl fmt::Debug for ErrorOnOpen {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{ File: {} at line {} }}", file!(), line!())
-    }
-}
-
-impl fmt::Display for ErrorOnRead {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Error when tried to read ROM file")
-    }
-}
-
-impl fmt::Debug for ErrorOnRead {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{ File: {} at line {} }}", file!(), line!())
-    }
-}
+impl FileErrorMessage for InvalidROMFile {}
+impl FileErrorMessage for ErrorOpeningROMFile {}
+impl FileErrorMessage for ErrorReadingROMFile {}
